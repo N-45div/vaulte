9b36077bef1dde4d9fb62a7027d5d61bcdd896b8
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2024-11-22 14:05:19 +0100

    feat: adding event counters.

--- contracts/router.sol
+++ contracts/router.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Interfaces/IMerchant.sol";
 import "./Interfaces/IUser.sol";
 import "./Interfaces/IUserFactory.sol";
@@ -14,11 +15,16 @@ import "./Interfaces/IInvestorPoolFactory.sol";
 
 contract Router is Ownable{
 
+    using Counters for Counters.Counter;
+    Counters.Counter public subscriptionCount;
+    Counters.Counter public subscriptionPaymentCount;
+    Counters.Counter public disbursedCount;
+
     address public userFactoryAddress;
 
-    event Subscription(address userAccount, address merchantAccount, uint256 tier, uint256 subTime);
-    event SubscriptionPayment(address userAccount, address merchantAccount, uint256 paymentAmount, uint256 paymentTime);
-    event LoanDisbursed(address investorAccount, address merchantAccount, uint256 amount, uint8 loanCategory);
+    event Subscription(uint256 indexed subscriptionId, address userAccount, address merchantAccount, uint256 tier, uint256 subTime);
+    event SubscriptionPayment(uint256 indexed paymentId, address userAccount, address merchantAccount, uint256 paymentAmount, uint256 paymentTime);
+    event LoanDisbursed(uint256 indexed disbursedId, address investorAccount, address merchantAccount, uint256 amount, uint8 loanCategory);
 
     constructor() Ownable(msg.sender) {}
 
@@ -36,7 +42,9 @@ contract Router is Ownable{
         IUser(userAccount).subscribe(tier, paymentDue, merchantAccount);
         IMerchant(merchantAccount).setSubscription(tier, paymentDue, userAccount);
 
-        emit Subscription(userAccount, merchantAccount, tier, block.timestamp);
+        uint256 subId = subscriptionCount.current();
+        subscriptionCount.increment();
+        emit Subscription(subId, userAccount, merchantAccount, tier, block.timestamp);
     }
 
     function fundRequest(address merchantAccount) external {
@@ -46,7 +54,9 @@ contract Router is Ownable{
         IMerchant(merchantAccount).receiveLoan(investorAccount);
         IInvestor(investorAccount).acceptRequest(merchantAccount, loanAmount, interest, loanPeriod, monthlyRepaymentAmount);
         // emit event
-        emit LoanDisbursed(investorAccount, merchantAccount, loanAmount, 0);
+        uint256 disbursedId = disbursedCount.current();
+        disbursedCount.increment();
+        emit LoanDisbursed(disbursedId, investorAccount, merchantAccount, loanAmount, 0);
     }
 
     function acceptOffer(address investorAccount, uint256 offerId) external {
@@ -55,7 +65,9 @@ contract Router is Ownable{
         uint256 repaymentAmount = monthlyRepaymentAmount * loanPeriod;
         IMerchant(merchantAccount).getLoan(investor, repaymentAmount, loanPeriod, monthlyRepaymentAmount);
         IInvestor(investor).disburseLoanOffer(merchantAccount, offerId);
-        emit LoanDisbursed(investorAccount, merchantAccount, loanAmount, 1);
+        uint256 disbursedId = disbursedCount.current();
+        disbursedCount.increment();
+        emit LoanDisbursed(disbursedId, investorAccount, merchantAccount, loanAmount, 1);
     }
 
     function getLoan(address investorPool, uint256 loanAmount) external {
@@ -66,12 +78,17 @@ contract Router is Ownable{
         uint256 monthlyRepaymentAmount = repaymentAmount / loanPeriod;
         IMerchant(merchantAccount).getLoan(investorPool, repaymentAmount, loanPeriod, monthlyRepaymentAmount);
         IInvestorPool(investorPool).getLoan(merchantAccount, loanAmount, loanPeriod);
-        emit LoanDisbursed(investorPool, merchantAccount, loanAmount, 2);
+        uint256 disbursedId = disbursedCount.current();
+        disbursedCount.increment();
+        emit LoanDisbursed(disbursedId, investorPool, merchantAccount, loanAmount, 2);
     }
 
     function charge(address userAccount, address merchantAccount, uint256 amount) external {
         IUser(userAccount).paySubscription(amount, merchantAccount);
-        emit SubscriptionPayment(userAccount, merchantAccount, amount, block.timestamp);
+        
+        uint256 paymentId = subscriptionPaymentCount.current();
+        subscriptionPaymentCount.increment();
+        emit SubscriptionPayment(paymentId, userAccount, merchantAccount, amount, block.timestamp);
     }
 
     function contributePool(address investorPool, uint256 amount) external {
